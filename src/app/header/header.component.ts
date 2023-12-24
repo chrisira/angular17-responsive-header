95b42e43074a50c56da7603c9cd3ce7a39affcad
Bishop <christianiradukunda6@gmail.com>
2023-12-24 22:09:50 +0200

    Initialized all the files

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+ menuValue:boolean=false;
+ menu_icon :string ='bi bi-list';
+ openMenu(){
+    this.menuValue =! this.menuValue ;
+    this.menu_icon = this.menuValue ? 'bi bi-x' : 'bi bi-list';
+  }
+   closeMenu() {
+    this.menuValue = false;
+    this.menu_icon = 'bi bi-list';
+  }
+}
